69ba230b79281a32a603629bdfdc2db4417c23ae
Nishith5b1 <nishith5b1@gmail.com>
2026-06-14 21:00:28 +0530

    loops

--- 07-for-loop/r53.tf
+++ 07-for-loop/r53.tf
@@ -14,5 +14,27 @@ resource "aws_route53_record" "frontend" {
   type = "A"
   zone_id = var.zone_id
   ttl = 1
+  #records = [aws_instance.instance_test[index(var.instances, "frontend")].public_ip]
   records = [lookup(aws_instance.instance_test, "frontend").public_ip] # equivalent to aws_instance.instance_test["frontend"]
 }
+
+#contains - works on list, so conatins(keys(map), value) will convert map in to list and searches for value
+#lookup - works on map, it takes input as map and key and get the value as output
+#index - works on list, it takes list & value and get's the index number
+
+
+#For list terraform creates
+
+# aws_instance.instance_test[0] -> mongodb
+# aws_instance.instance_test[1] -> redis
+# aws_instance.instance_test[2] -> mysql
+# ...
+# aws_instance.instance_test[9] -> frontend
+
+# For map terraform creates
+
+# aws_instance.roboshop["mongodb"]
+# aws_instance.roboshop["frontend"]
+
+# Resources are indexed by KEY
+# not by NUMBER
